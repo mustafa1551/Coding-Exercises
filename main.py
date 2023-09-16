4bf018e166de16509cbf6ef7c974b086736f3061
mustafa1551 <145202996+mustafa1551@users.noreply.github.com>
2023-09-17 00:23:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#new_two_digit_number = int(two_digit_number)
+x = (two_digit_number)
+y = x[0]
+z = x[1]
+first_digit = int(y)
+second_digit = int(z)
+print(first_digit + second_digit)
