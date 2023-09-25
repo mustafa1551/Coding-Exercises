e7754a8feecdeff7ffa976aa9034ae14faf8fe38
mustafa1551 <145202996+mustafa1551@users.noreply.github.com>
2023-09-25 22:23:41 +0300

    Add files via upload

---
+++ treasure_hunt_fever_dream_edition.py
@@ -0,0 +1,45 @@
+#This is a dumb game I made, It is a different version of treasure island game, featuring taylor swift, keanu reeves, morpheus and a dinosaur
+print("Welcome to the treasure hunt : FEVER DREAM EDITION")
+choice_1 = input("\nYou are at a crossroad, and there are two roads ahead of you, and on the right one Taylor swift waves at you, on the left one however Keanu Reeves waiting in a mustang, which one do you choose? type left or right ? ")
+choice_1 = choice_1.lower()
+if choice_1 == "left":
+   print("""
+You are cruising with Keanu on the driver side, after you two vibe it off for a while.
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡤⢤⣶⣶⣶⣶⣶⣒⣒⣀⣺⣿⣿⠿⢶⣶⣶⣶⣦⣤⣤⣤⣄⣀⣀⣀⣀⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠛⠒⠲⠦⢤⣉⠙⣿⣿⣿⣟⢿⣿⠿⠿⠿⢿⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣷⡦⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⡇⠀⠈⠛⢿⣿⡀⠀⠀⠀⠻
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⠤⢴⣿⣉⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⠀⠀⠀⠀⠙⣿⣄⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣤⡶⠞⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠓⠒⠒⠢⠤⠤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠀⢠⠤⠤⠤⣤⣾⠀⠙⢦⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⠴⣾⣛⣩⢴⣿⠿⠶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠙⠒⠒⠒⠤⠤⢤⣤⣾⢿⣴⢏⣀⣀⣤⡼⠻⡆⠀⠈⢷
+⠀⠀⠀⠀⠀⢀⣴⠋⠁⠀⠀⠀⠀⠙⠓⠲⠤⠬⠷⠀⠀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠤⡞⠉⠀⠀⠹⡟⠀⠀⠀⠀⠀⢱⠀⢠⣼
+⠀⠀⠀⣀⡴⠋⠈⢙⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠢⠤⢄⣀⣠⠴⠛⣋⣠⠴⠒⠉⠉⢉⣲⠶⠀⣀⡠⠤⠒⠊⠉⠁⠀⠀⠀⠠⠀⡂⠐⢹⠂⠀⠀⠀⠀⣼⢰⣿⣿
+⡀⣠⣼⠏⠀⠀⣰⠟⠦⢤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠓⠒⠒⠢⠤⠤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠀⢠⠤⠤⠤⣤⣾⠀⠙⢦⡀
+⡟⠋⠹⢤⣠⠞⠁⠀⠀⠀⠀⠈⠉⠐⠲⠤⢄⣀⡀⠀⠀⠀⠀⣀⠤⠤⠤⠤⠤⣄⣀⣀⠀⢀⡠⠖⠋⠀⠀⠀⠄⡠⢐⠤⠀⠂⠀⣀⣀⡀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⣿⣿⣿⣿
+⢻⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢒⡶⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⢦⠀⠀⠀⠈⠈⠀⠀⠀⠀⢀⣴⣿⡿⣿⡝⣆⠀⠀⠀⠴⣽⠤⠞⣠⣵⠿⣻⣿⣿⠈
+⢿⣿⣿⠶⢤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⠇⠀⠀⠀⠀⣀⣀⣤⣤⣾⣿⣿⣿⢻⢸⣿⣾⣿⣤⣾⣿⠿⢋⡡⠞⠉⠉⠉⠉⠀
+⢸⣿⣿⡇⠀⠘⣿⣶⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠈⠑⠲⠤⢤⣀⣀⡀⠀⠀⠀⠀⠀⠀⣀⣀⣞⣀⣠⣤⣾⣿⣿⣿⣿⣿⣿⣿⢻⢸⣿⣾⣿
+
+
+""")
+   choice_2 = input("But wait, what is that? Is that a dinosaur up ahead on the road? Oh no, it starts approaching towards you...\nWhat do you do? You can either 'run' or 'wait'.\nPlease type 'run' or 'wait' to make a decision: ").lower()
+   if choice_2 =="run":
+    print("Buddy what were you thinking? THAT YOU WERE GONNA OUTRUN A DINOSAUR.\n Get your head in the game and try again.")
+   elif choice_2 == "wait":
+    print("""The dinosaur approaches but Keanu just pops a wink and dinosaur thinks what a chill guy and gives a thumbs up leaves.\n
+    you guys continue on the road""")
+    choice_3 = input("""What is that 'Morpheus' appears. He asks you to choose between blue pill and red pill, what you do?\n
+    please type blue or red to answer.""").lower()
+    if choice_3 == "blue":
+     print("Congrats you won take the blue pill Keanu, You and the Dinosaur suddenly appear at a  beach at the Carribeans and start vibing \n You've won")
+    elif choice_3 == "red":
+     print("Morpheus turns into Taylor Swift and eats you alive.\n Try again :)")
+
+  
+elif choice_1 == "right":
+     print("""  
+     You go with TayTay on this one,  i get it, however: \n
+     You two date for a while but ended up with a bad break up, you are now a great inspiration for her upcoming song\n 
+     BAD BLOOOOD 🎵🎵🎵🎵🎵🎵🎵🎵🎵🎵 \n
+     Better luck next time buddy :)""")
+  
+
